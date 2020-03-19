ef718e1ebb8d86b32c3e748eddb7aee6e56fa223
Nick Walker <nickwalk@microsoft.com>
2020-03-19 12:57:29 -0700

    updated response object

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -3,7 +3,17 @@ const questions = require("../questions");
 module.exports = async function(context, req) {
   context.log("Request for all questions");
 
+  // context.res = {
+  //    body: questions
+  // };
+
   context.res = {
-    body: questions
+    body: questions.map(q => {
+      return {
+        question: q.question,
+        answers: q.incorrect_answers.concat([q.correct_answer]).sort()
+      };
+    })
   };
+  
 };
